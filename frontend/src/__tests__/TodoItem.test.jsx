caadea3de12e1a0fdb93149b40b7d3cd0a317963
Nipitpon Nontem <nipitponnon9@gmail.com>
2026-03-09 12:22:54 +0700

    vite(2)

--- frontend/src/__tests__/TodoItem.test.jsx
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -5,15 +5,29 @@ import TodoItem from '../TodoItem.jsx'
 const baseTodo = {             // ** TodoItem พื้นฐานสำหรับทดสอบ
   id: 1,
   title: 'Sample Todo',
+  done: false,
   comments: [],
 };
 
 describe('TodoItem', () => {
-  it('renders with no comments correctly', () => {    
+     it('renders with no comments correctly', () => {    
     // *** โค้ดสำหรับเทสที่เพิ่มเข้ามา
-    render(
-      <TodoItem todo={baseTodo} />
-    );
+        render(
+          <TodoItem todo={baseTodo} />
+        );
+        expect(screen.getByText('Sample Todo')).toBeInTheDocument();
+    });
+    it('renders with comments correctly', () => {
+        const todoWithComment = {
+        ...baseTodo,
+            comments: [
+            {id: 1, message: 'First comment'},
+            {id: 2, message: 'Another comment'},
+        ]
+        };
+        render(
+            <TodoItem todo={todoWithComment} />
+            );
     expect(screen.getByText('Sample Todo')).toBeInTheDocument();
-  });
+    });
 });
